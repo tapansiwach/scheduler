377c53e139c7b672de4e021e76993a7008e28fb8
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-29 05:56:31 -0400

    use props values for the InterviewerList component

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 
 import "components/InterviewerList.scss"
+import InterviewerListItem from "./InterviewerListItem";
 
-export default function InterviewerList() {
+export default function InterviewerList(props) {
+  const parsedInterviewers = props.interviewers.map(interviewer => (
+    <InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={props.setInterviewer}
+      selected={interviewer.id === props.interviewer}
+    />
+  ));
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {parsedInterviewers}
+      </ul>
     </section>
   );
 }
