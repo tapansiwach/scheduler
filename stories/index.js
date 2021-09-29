c12abc31fdf65ea887acc4c2026f7b59460d6b8b
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-29 17:01:54 -0400

    code fix: fix reference error & add stofy for Error component

--- stories/index.js
+++ stories/index.js
@@ -15,7 +15,8 @@ import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
-import Status from "components/Status";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -155,4 +156,5 @@ storiesOf("Appointment", module)
     onCancel={action("onCancel")}
     onConfirm={action("onConfirm")}
   />)
-  .add("Status", () => <Status message="Deleting" />);
+  .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => <Error message="Could not delete appointment." />);
