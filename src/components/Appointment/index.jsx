4e191025a8df1acde4419c1cd492120fbe864a29
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-29 19:59:22 -0400

    add test data for Appointments

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -8,6 +8,53 @@ import Confirm from "./Confirm";
 import Status from "./Status";
 import Error from "./Error";
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  }
+];
+
 export default function Appointment(props) {
   return (
     <article className="appointment">
