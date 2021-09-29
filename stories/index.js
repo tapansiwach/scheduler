96e9e7706ec82995fc3b5508ca7e5280b0769201
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-29 06:34:09 -0400

    update the 'Clickable' story of the InterviewListItem

--- stories/index.js
+++ stories/index.js
@@ -98,9 +98,9 @@ storiesOf("InterviewerListItem", module)
       id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={action("setInterviewer")}
+      setInterviewer={event => action("setInterviewer")(interviewer.id)}
     />
-  ));
+  ));;
 
 
 const interviewers = [
