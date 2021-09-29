d519d4d470653b9d6a7289f9ec03a83245130102
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-29 18:54:27 -0400

    update the Form component to add the name and interviewer to state using setName and setInterviewer

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -17,7 +20,7 @@ export default function Form(props) {
           */
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={props.interviewer} onChange={props.setInterviewer} />
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
