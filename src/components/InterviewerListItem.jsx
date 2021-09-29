9ced30468ed8ede55398a8ae310d6ea07aac328d
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-29 05:37:11 -0400

    use props instead of hardcoded values for InterviewerListItem component

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import classnames from "classnames"
 import "components/InterviewerListItem.scss"
 
 export default function InterviewerListItem(props) {
+  const liClassNames = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+
+  const imgClassNames = classnames("interviewers__item-image", {
+    "interviewers__item--selected-image": props.selected
+  });
+
   return (
-    <li className="interviewers__item">
+    <li className={liClassNames}>
       <img
-        className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        className={imgClassNames}
+        src={props.avatar}
+        alt={props.name}
+        onClick={props.setInterviewer}
       />
-      Sylvia Palmer
+      {props.name}
     </li>
   );
 }
