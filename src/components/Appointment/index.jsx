ddf85064fd3afac0eb0690bb01c193b4136099a3
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-29 17:07:24 -0400

    import Appointment components into index.jsx

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
 import "components/Appointment/styles.scss"
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
+import Confirm from "./Confirm";
+import Status from "./Status";
+import Error from "./Error";
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+    </article>
   );
 }
