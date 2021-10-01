dd411e054d4a1124ea442787557e94ea2cf8f888
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-01 09:42:37 -0400

    ensure that useVisualMode can transition to another mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,5 +3,12 @@ import React, { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
-  return { mode };
+  function transition(subsequent) {
+    setMode(subsequent);
+  }
+
+  return {
+    mode,
+    transition
+  };
 }
