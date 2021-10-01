fecace18b45e4de615dc7ddc7ef20a514791e24b
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-01 16:03:47 -0400

    implement the back action

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,10 +6,19 @@ export default function useVisualMode(initial) {
 
   function transition(subsequent) {
     setMode(subsequent);
+    setHistory(prev => [...prev, subsequent])
   }
 
   function back() {
-
+    if (history.length > 0) {
+      // make a copy of history so we can pop from it directly instead of having to use setHistory
+      const copy = [...history];
+      copy.pop();
+      // change history using setHistory
+      setHistory(copy);
+      // setMode using the copy because setHistory may not have executed immediately
+      setMode(copy[copy.length - 1]);
+    }
   }
 
   return {
