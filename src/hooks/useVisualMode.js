3e325390c583001ea3903a2237fd0c84d7a6a913
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-01 14:34:24 -0400

    keep track of the history of the modes, so we can go backwords

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   function transition(subsequent) {
     setMode(subsequent);
