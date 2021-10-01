00fbf35489a757ac0149a58998a147c1d11fd66a
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-01 17:47:28 -0400

    implement the code needed to pass the test for transitioning with replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,9 +4,19 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(subsequent) {
-    setMode(subsequent);
-    setHistory(prev => [...prev, subsequent])
+  function transition(subsequent, replace) {
+    // if it's a replace request...
+    if (replace) {
+      // delete the last item before adding to the history
+      const copy = [...history];
+      copy.pop();
+      setMode(subsequent);
+      setHistory([...copy, subsequent])
+    } else { // if it's not a replace request
+      // add the item to history
+      setMode(subsequent);
+      setHistory(prev => [...prev, subsequent])
+    }
   }
 
   function back() {
