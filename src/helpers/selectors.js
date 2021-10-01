4c2af497dc49075d777dcfcaaa82baac45ed2bc6
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-01 06:06:17 -0400

    implement the getInterview function to pass the tests which were earlier setup for it

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -25,9 +25,23 @@ export function getAppointmentsForDay(state, day) {
 
 
 export function getInterview(state, interview) {
+  if (interview === null) return null;
 
+  // get an array of interviewers
+  const interviewers = Object.values(state.interviewers);
+
+  // match the interviewer object which has the required id
+  const filtered = interviewers.filter(x => x.id === interview.interviewer)
+
+  // if no match is found, return null
+  if (filtered.length === 0) return null;
+
+  // return the desired interview object
+  return {
+    "student": interview.student,
+    "interviewer": filtered[0]
+  }
 }
-
 /* (sample) returned object looks like this
 {
   "student": "Lydia Miller-Jones",
