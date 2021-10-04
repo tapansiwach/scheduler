f7388955ec7bac202fccc3c328a82e47ddb77818
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-04 02:35:27 -0400

    add the appointment to Application's list of appointments when an interview is booked

--- src/components/Application.js
+++ src/components/Application.js
@@ -20,7 +20,19 @@ export default function Application(props) {
   let dailyAppointments = []
 
   function bookInterview(id, interview) {
-    console.log(id, interview);
+    // console.log(id, interview);
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    }
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    }
+
+    setState({ ...state, appointments });
   }
 
   useEffect(() => {
