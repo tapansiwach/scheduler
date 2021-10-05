e37d55a67e24224eaec5d05e842330fd3cef1c47
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-05 12:40:56 -0400

    add incomplete tests to verify form behaviour: student name should not be blank

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -26,4 +26,24 @@ describe("Form", () => {
     );
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
+
+  it("validates that the student name is not blank", () => {
+    /* 1. validation is shown */
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+
+    /* 2. onSave is not called */
+    expect(onSave).not.toHaveBeenCalled();
+  });
+
+  it("calls onSave function when the name is defined", () => {
+    /* 3. validation is not shown */
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+
+    /* 4. onSave is called once*/
+    expect(onSave).toHaveBeenCalledTimes(1);
+
+    /* 5. onSave is called with the correct arguments */
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+  });
+
 });
