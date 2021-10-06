a953e72499b00de903b1cfddc49286eaa4d13730
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-06 13:37:06 -0400

    implement testing the booking of an interview

--- cypress/integration/interview.specs.js
+++ cypress/integration/interview.specs.js
@@ -3,11 +3,22 @@ describe("Interview Appintment", () => {
   it("should book an interview", () => {
 
     // Visits the root of web server
+    cy.visit("/")
     // Clicks on the "Add" button in the second appointment
+    cy.get(":nth-child(2) > .appointment__add > .appointment__add-button")
+      .click()
     // Enters their name
+    cy.get("[data-testid=student-name-input]")
+      .type("Archie Cohen")
     // Chooses an interviewer
+    cy.get(".interviewers__item-image").first()
+      .click()
     // Clicks the save button
+    cy.get(".button--confirm")
+      .click()
     // Sees the booked appointment
+    cy.get(".appointment__card-left > h2.text--regular")
+      .contains("Archie Cohen")
 
   });
 
