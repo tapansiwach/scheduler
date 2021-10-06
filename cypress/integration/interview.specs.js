68f9d6f492cb28c7c93ad517a4e66498b1a8434e
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-06 14:16:53 -0400

    implement testing of cancelling an interview

--- cypress/integration/interview.specs.js
+++ cypress/integration/interview.specs.js
@@ -17,7 +17,7 @@ describe("Interview Appintment", () => {
     cy.get(".button--confirm")
       .click()
     // Sees the booked appointment
-    cy.get(".appointment__card-left > h2.text--regular")
+    cy.get(":nth-child(2) > .appointment__card > .appointment__card-left > h2.text--regular")
       .contains("Archie Cohen")
 
   });
@@ -25,19 +25,38 @@ describe("Interview Appintment", () => {
   it("should edit an interview", () => {
 
     // Visits the root of web server
+    cy.visit("/")
     // Clicks the edit button for the existing appointment
+    cy.get(":nth-child(2) > .appointment__card [alt='Edit']")
+      .click({ force: true })
     // Changes the name and interviewer
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Jughead")
+    cy.get(".interviewers__item-image").last()
+      .click()
     // Clicks the save button
-    // Sees the edit to the appointment
+    cy.get(".button--confirm")
+      .click()
+    // Sees the booked appointment
+    cy.get(":nth-child(2) > .appointment__card > .appointment__card-left > h2.text--regular")
+      .contains("Jughead")
 
   });
 
   it("should cancel an interview", () => {
 
     // Visits the root of web server
+    cy.visit("/")
     // Clicks the delete button for the existing appointment
+    cy.get(":nth-child(2) > .appointment__card [alt='Delete']")
+      .click({ force: true })
     // Clicks the confirm button
+    cy.contains(".button--danger", "Confirm")
+      .click()
     // Sees that the appointment slot is empty
+    cy.get(":nth-child(2)")
+      .should("have.class", "appointment__add")
 
   });
 
