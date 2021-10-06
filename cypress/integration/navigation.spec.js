a2b054293f85c4c40a393381fc7e6ddcb0b4a404
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-06 09:30:35 -0400

    add a cypress test to navigate to Tuesday

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,12 @@
 describe("Navigation", () => {
+
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should visit Tuesday", () => {
+    cy.visit("/");
+    cy.get("li").contains("Tuesday").click();
+  });
+
 });
