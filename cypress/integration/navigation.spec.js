842d19bc63ee406856e548e9fa66d631456287c7
Tapan Siwach <siwach.tapan@gmail.com>
2021-10-06 09:45:09 -0400

    check using cypress that the color of list item containing Tuesday is off-white

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -9,4 +9,14 @@ describe("Navigation", () => {
     cy.get("li").contains("Tuesday").click();
   });
 
+  // #f2f2f2
+
+  it("bakground color of Tuesday should be offwhite", () => {
+    cy.visit("/");
+    cy.get("li").contains("Tuesday").click();
+
+    cy.contains("li", "Tuesday")
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+  });
+
 });
